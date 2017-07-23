3b6ee71f16124c31f46aaa34d59b1fda9d364831
Artem Eroshenko <eroshenkoam@qameta.io>
2017-07-23 18:43:20 +0300

    Initial commit

---
+++ src/main/java/io/eroshenkoam/Main.java
@@ -0,0 +1,13 @@
+package io.eroshenkoam;
+
+/**
+ * eroshenkoam
+ * 23.07.17
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
